2ff3e698ca3c25164b1b2a1fbf802f983bd19ade
iker acevedo <iker.acevedo24@campusfp.es>
2025-10-06 10:40:20 +0200

    Subida de manejo de excepciones

---
+++ ASP-A1.2-EjerciciosManejoExcepciones/Ejercicio_1/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Objetivo: Usar try/catch y TryParse.
+Descripción: pedir un número, convertirlo a entero de forma segura usando TryParse y manejar números negativos con try/catch.
+Entrada de usuario:
+Ingresa un número: abc
+Ingresa un número: -5
+Ingresa un número: 10
+Salida de consola:
+Valor no válido
+Error: El número no puede ser negativo
+Número aceptado: 10
+*/
+
+class Ejercicio_1
+{
+    static void Main(string[] args)
+    {
+
+        Conversion();
+    }
+
+    static int Conversion()
+    {
+        while (true)
+        {
+            Console.WriteLine("Dime un numero: ");
+            string numero = Console.ReadLine();
+            if(int.TryParse(numero, out int salida) &&  salida >= 0)
+            {
+                Console.WriteLine("Numero aceptado: " + salida);
+                return salida;
+               
+            }
+            else
+            {
+                Console.WriteLine("Numero no valido");
+            }
+        }
+    }
+}
---
+++ ASP-A1.2-EjerciciosManejoExcepciones/Ejercicio_2/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Objetivo: Usar Dictionary.TryGetValue.
+Descripción: crear un diccionario con nombres de alumnos y edades, y buscar un nombre de forma segura usando TryGetValue.
+Diccionario:
+Ana → 25
+Luis → 30
+Entrada de usuario:
+Ingresa nombre del alumno: Ana
+Ingresa nombre del alumno: Pedro
+
+
+Salida de consola:
+Edad: 25
+Alumno no encontrado
+*/
+
+class Ejercicio_2
+{
+    static void Main(string[] args)
+    {
+        var dic = new Dictionary<string, int>
+        {
+            { "iker", 22 },
+            { "rafa", 25 }
+        };
+
+        for (int i = 0; i < 2; i++)
+        {
+            Console.WriteLine("Ingresa el nombre del alumno: ");
+            string nombre = Console.ReadLine();
+
+            if (dic.TryGetValue(nombre, out int edad))
+                Console.WriteLine($"Edad: {edad}");
+            else
+                Console.WriteLine("Alumno no encontrado");
+        }
+    }
+}
